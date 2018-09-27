ea710d7ba82e1c80f901caedf552ad04c65f1783
TheDestroyer19 <TheDestroyer19@github.com>
2018-09-26 20:44:47 -0600

    Improved understanding and use of the pca9685's registers.  Also fixed deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#move to right dir
+pwd
+
+#build for armv6
+echo "Building" &&
+cargo +nightly build --target=arm-unknown-linux-gnueabihf &&
+
+#copy file to target system (pi@raspberrypi.local)
+echo "Tranfering to pi@raspberrypi.local" &&
+scp target/arm-unknown-linux-gnueabihf/debug/rust_tank_v2 pi@raspberrypi.local:~/debug_rust_tank &&
+
+#tell remote to start program
+echo "Starting Program" &&
+ssh -t pi@raspberrypi.local "RUST_BACKTRACE=1 ./debug_rust_tank"
