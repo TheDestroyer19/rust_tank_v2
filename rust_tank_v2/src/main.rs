2d8f0b4dc802133b341b8bc25df5983375d914e0
TheDestroyer19 <17345449+TheDestroyer19@users.noreply.github.com>
2018-10-15 14:13:38 -0600

    Separated I2C communication into single dedicated thread

--- rust_tank_v2/src/main.rs
+++ rust_tank_v2/src/main.rs
@@ -10,13 +10,15 @@ use std::f32::consts::PI;
 
 use termion::event::Key;
 
+use real_time_interface::{RTHandle, RTCommand};
+
 mod real_time_interface;
 //Old modules below
-mod motors;
-use motors::Motors;
-mod sensors;
-use sensors::Sensors;
-mod hw_tests;
+//mod motors;
+//use motors::Motors;
+//mod sensors;
+//use sensors::Sensors;
+//mod hw_tests;
 mod terminal;
 use terminal::{ InputError};
 
@@ -25,21 +27,17 @@ fn main() {
 
 
     //initialize hardware
-    pca9685::PCA9685::software_reset().unwrap();
-    let mut motors = Motors::new().unwrap();
-    motors.set_turret(0).unwrap();
-    let mut sensors = Sensors::new().unwrap();
-    
+    let mut interface = real_time_interface::RTHandle::initialize().unwrap();
 
-    match run(&mut motors, &mut sensors) {
+    match run(&mut interface) {
         Err(e) => println!("{}", e),
         _ => (),
     };
-    //hw_tests::run_all_tests(&mut motors);
-    motors.stop().unwrap();
+
+    interface.close();
 }
 
-fn run(motors: &mut Motors, sensors: &mut Sensors) -> std::io::Result<()> {
+fn run(interface: &mut RTHandle) -> std::io::Result<()> {
     let (mut input, mut output) = terminal::new()?;
 
     output.draw_static()?;
@@ -55,24 +53,24 @@ fn run(motors: &mut Motors, sensors: &mut Sensors) -> std::io::Result<()> {
                 Key::Char('w') => {
                         speed += 0.25;
                         if speed > 1.0 {speed = 1.0; }
-                        update_drive(motors, speed, turn)
+                        update_drive(interface, speed, turn)
                     },
                 Key::Char('s') => {
                         speed -= 0.25;
                         if speed < -1.0 {speed = -1.0; }
-                        update_drive(motors, speed, turn)
+                        update_drive(interface, speed, turn)
                     },
                 Key::Char('a') => {
                         turn -= 0.25;
                         if turn < -1.0 {turn = -1.0; }
-                        update_drive(motors, speed, turn)
+                        update_drive(interface, speed, turn)
                     },
                 Key::Char('d') => {
                         turn += 0.25;
                         if turn > 1.0 {turn = 1.0; }
-                        update_drive(motors, speed, turn)
+                        update_drive(interface, speed, turn)
                     },
-                Key::Char('q') => {
+                /*Key::Char('q') => {
                     degrees -= 5;
                     if degrees < -90 {degrees = -90; }
                     motors.set_turret(degrees)
@@ -81,7 +79,7 @@ fn run(motors: &mut Motors, sensors: &mut Sensors) -> std::io::Result<()> {
                     degrees += 5;
                     if degrees > 90 {degrees = 90; }
                     motors.set_turret(degrees)
-                },
+                },*/
                 _ => Ok(()),
             },
             Ok(None) => Ok(()),
@@ -90,14 +88,13 @@ fn run(motors: &mut Motors, sensors: &mut Sensors) -> std::io::Result<()> {
             Ok(_) => (),
             Err(e) => { output.print_error(e)? },
         };
-        match sensors.update() {
-            Ok(_) => (),
-            Err(e) => { output.print_error(e)? },
+        for err in interface.update() {
+            output.print_error(err)?;
         }
         output.draw_motors(speed, turn, degrees)?;
         output.draw_sensors(
-            sensors.get_accel(), sensors.get_gyro(),
-            sensors.get_pitch() * 180.0 / PI, sensors.get_roll() * 180.0 / PI,
+            interface.raw_state().accel, interface.raw_state().gyro,
+            interface.pitch() * 180.0 / PI, interface.roll() * 180.0 / PI,
         )?;
         thread::sleep(Duration::from_millis(10));
     }
@@ -105,7 +102,7 @@ fn run(motors: &mut Motors, sensors: &mut Sensors) -> std::io::Result<()> {
     Ok(())
 }
 
-fn update_drive(motors: &mut Motors, speed: f32, turn: f32) -> std::io::Result<()> {
+fn update_drive(interface: &mut RTHandle, speed: f32, turn: f32) -> std::io::Result<()> {
     let mut ls = speed;
     let mut rs = speed;
     //TODO use turn
@@ -116,7 +113,58 @@ fn update_drive(motors: &mut Motors, speed: f32, turn: f32) -> std::io::Result<(
         ls = turn;
         rs = -turn;
     }
-    motors.set_drive_left(ls)?;
-    motors.set_drive_right(rs)?;
+    //TODO TEMP CODE, Make this implemented in the interface to the hardware
+    // or abstracted as commands
+    const MOT_LPWM: u8 = 15;
+    const MOT_LA: u8 = 13;
+    const MOT_LB: u8 = 14;
+    const MOT_RPWM: u8 = 10;
+    const MOT_RA: u8 = 11;
+    const MOT_RB: u8 = 12;
+    if ls > 0.0 {
+        interface.send_command(RTCommand::SetPwmOn(MOT_LA));
+        interface.send_command(RTCommand::SetPwmOff(MOT_LB));
+        interface.send_command(RTCommand::SetPwm{
+            pwm: MOT_LPWM,
+            on: 0,
+            off: (4095.0 * ls.abs()) as u16,
+        });
+    } else if ls < 0.0 {
+        interface.send_command(RTCommand::SetPwmOn(MOT_LB));
+        interface.send_command(RTCommand::SetPwmOff(MOT_LA));
+        interface.send_command(RTCommand::SetPwm{
+            pwm: MOT_LPWM,
+            on: 0,
+            off: (4095.0 * ls.abs()) as u16,
+        });
+    } else {
+        interface.send_command(RTCommand::SetPwmOff(MOT_LB));
+        interface.send_command(RTCommand::SetPwmOff(MOT_LA));
+        interface.send_command(RTCommand::SetPwmOff(MOT_LPWM));
+    }
+    if rs > 0.0 {
+        interface.send_command(RTCommand::SetPwmOn(MOT_RA));
+        interface.send_command(RTCommand::SetPwmOff(MOT_RB));
+        interface.send_command(RTCommand::SetPwm{
+            pwm: MOT_RPWM,
+            on: 0,
+            off: (4095.0 * rs.abs()).round() as u16,
+        });
+    } else if rs < 0.0 {
+        interface.send_command(RTCommand::SetPwmOn(MOT_RB));
+        interface.send_command(RTCommand::SetPwmOff(MOT_RA));
+        interface.send_command(RTCommand::SetPwm{
+            pwm: MOT_RPWM,
+            on: 0,
+            off: (4095.0 * rs.abs()).round() as u16,
+        });
+    } else {
+        interface.send_command(RTCommand::SetPwmOff(MOT_RB));
+        interface.send_command(RTCommand::SetPwmOff(MOT_RA));
+        interface.send_command(RTCommand::SetPwmOff(MOT_RPWM));
+    }
+    //END TEMP CODE
+    //motors.set_drive_left(ls)?;
+    //motors.set_drive_right(rs)?;
     Ok(())
 }
--- rust_tank_v2/src/real_time_interface.rs
+++ rust_tank_v2/src/real_time_interface.rs
@@ -15,12 +15,103 @@ use mpu6050::MPU6050;
 
 const PWM_FREQ: f32 = 120.0;
 
+/// Interface to the real time thread
+/// This is currently responsible for managing communication with i2c devices
 pub struct RTHandle {
-    pub rx: Receiver<Result<SensorState, LinuxI2CError>>,
-    pub tx: Sender<RTCommand>,
+    rx: Receiver<Result<SensorState, LinuxI2CError>>,
+    tx: Sender<RTCommand>,
     handle: JoinHandle<()>,
+    raw_state: SensorState,
+    pitch: f32,
+    roll: f32,
 }
 
+impl RTHandle {
+    /// Instantiates the interface between the non-critital timing
+    /// portions of the controller, and the timing critical portions. This sets
+    /// up the initial state of the I2C devices, and starts the
+    /// thread that controls them.
+    pub fn initialize() -> Result<RTHandle, LinuxI2CError> {
+        // initialize PWM hardware
+        let mut pca = PCA9685::default()?;
+        pca.set_all_pwm_off()?;
+        pca.set_pwm_freq(PWM_FREQ)?;
+        // initialize MPU hardware
+        let mut mpu = MPU6050::new(0x68)?;
+        mpu.set_accel_range(mpu6050::ACCEL_RANGE_2G)?;
+        mpu.set_gyro_range(mpu6050::GYRO_RANGE_250DEG)?;
+        // setup communication channels
+        let (rt_tx, rx) = mpsc::channel();
+        let (tx, rt_rx) = mpsc::channel();
+        // setup the real time thread
+        //TODO ensure that this thread runs when its asked to.
+        let handle = thread::spawn(|| real_time_loop(pca, mpu, rt_tx, rt_rx));
+
+        Ok(RTHandle {
+            rx, tx,
+            handle: handle,
+            raw_state: SensorState::default(),
+            roll: 0.0,
+            pitch: 0.0,
+        })
+    }
+
+    /// Goes through all state updates received, and updates the current state
+    /// Returns any IO errors that occured on the other thread since last call to update
+    pub fn update(&mut self) -> Vec<LinuxI2CError> {
+        let mut errors = vec![];
+        'queue: loop {
+            match self.rx.try_recv() {
+                Ok(Ok(new_state)) => {
+                    //TODO do processing on state
+                    let (x, y, z) = new_state.accel;
+                    self.roll = y.atan2(z);
+                    self.pitch = (-x / (y * self.roll.sin() + z * self.roll.cos())).atan();
+                    self.raw_state = new_state;
+                },
+                Ok(Err(err)) => {
+                    errors.push(err);
+                },
+                Err(TryRecvError::Empty) => break 'queue, //pipe empty
+                Err(TryRecvError::Disconnected) => {
+                    panic!("Real time thread aborted!");
+                },
+            }
+        }
+        errors
+    }
+
+    /// Sends a command to an I2C device
+    pub fn send_command(&mut self, command: RTCommand) {
+        //shouldn't panic unless the other thread terminates
+        if let Err(_) =self.tx.send(command) {
+            //TODO ensure motors are stopped
+            //If the real time thread ends (or drops the communication pipe
+            //then we cannot recover, except maybe make the HW enter a safe state.
+            panic!("Real time thread aborted!");
+        }
+    }
+
+    pub fn raw_state(&self) -> &SensorState {
+        &self.raw_state
+    }
+
+    pub fn pitch(&self) -> f32 {
+        self.pitch
+    }
+
+    pub fn roll(&self) -> f32 {
+        self.roll
+    }
+
+    pub fn close(mut self) {
+        self.send_command(RTCommand::StopAllMotors);
+        self.send_command(RTCommand::End);
+        self.handle.join().expect("Real time thread paniced!");
+    }
+}
+
+/// Possible commands for i2d devices
 pub enum RTCommand {
     SetPwm {
         pwm: u8,
@@ -29,37 +120,28 @@ pub enum RTCommand {
     },
     SetPwmOff(u8),
     SetPwmOn(u8),
-    StopAllMotors(),
+    StopAllMotors,
+    /// Terminates the real time thread, should NOT be used outside of the close method.
     End,
+    //TODO consider creating an enum fof each i2c device individually
 }
 
 pub struct SensorState  {
-    time: SystemTime,
-    gyro: (f32, f32, f32),
-    accel: (f32, f32, f32),
-    temp: f32,
+    pub time: SystemTime,
+    pub gyro: (f32, f32, f32),
+    pub accel: (f32, f32, f32),
+    pub temp: f32,
     //TODO should PWM state be included?
 }
-
-pub fn initialize() -> Result<RTHandle, LinuxI2CError> {
-    // initialize PWM hardware
-    let mut pca = PCA9685::default()?;
-    pca.set_all_pwm_off()?;
-    pca.set_pwm_freq(PWM_FREQ)?;
-    // initialize MPU hardware
-    let mut mpu = MPU6050::new(0x68)?;
-    mpu.set_accel_range(mpu6050::ACCEL_RANGE_2G)?;
-    mpu.set_gyro_range(mpu6050::GYRO_RANGE_250DEG)?;
-    // setup communication channels
-    let (rt_tx, rx) = mpsc::channel();
-    let (tx, rt_rx) = mpsc::channel();
-    // setup the real time thread
-    //TODO ensure that this thread runs when its asked to.
-    let handle = thread::spawn(|| real_time_loop(pca, mpu, rt_tx, rt_rx));
-
-    Ok(RTHandle {
-        rx, tx, handle: handle,
-    })
+impl Default for SensorState {
+    fn default() -> SensorState {
+        SensorState {
+            time: SystemTime::now(),
+            gyro: (0.0, 0.0, 0.0),
+            accel: (0.0, 0.0, 0.0),
+            temp: 0.0,
+        }
+    }
 }
 
 fn real_time_loop(mut pca: PCA9685, mut mpu: MPU6050,
@@ -71,16 +153,18 @@ fn real_time_loop(mut pca: PCA9685, mut mpu: MPU6050,
         if let Err(_) = tx.send(collect_data(&mut mpu, &mut pca, time.clone())) {
             return; //Main thread ended / dropped the handle
         }
-        if let Err(e) = match rx.try_recv() {
-            Err(TryRecvError::Empty) => Ok(()), //nothing to do
-            Err(TryRecvError::Disconnected) => return, //Main thread ended / dropped the handle
-            Ok(RTCommand::SetPwm {pwm, on, off}) => pca.set_pwm(pwm, on, off),
-            Ok(RTCommand::SetPwmOff(pwm)) => pca.set_pwm_off(pwm),
-            Ok(RTCommand::SetPwmOn(pwm)) => pca.set_pwm_on(pwm),
-            Ok(RTCommand::StopAllMotors()) => pca.set_all_pwm_off(),
-            Ok(RTCommand::End) => return, //Main thread asked us to stop
-        } {
-            if let Err(_) = tx.send(Err(e)) { return; }
+        'commands: loop {
+            if let Err(e) = match rx.try_recv() {
+                Err(TryRecvError::Empty) => break 'commands, //nothing to do
+                Err(TryRecvError::Disconnected) => return, //Main thread ended / dropped the handle
+                Ok(RTCommand::SetPwm {pwm, on, off}) => pca.set_pwm(pwm, on, off),
+                Ok(RTCommand::SetPwmOff(pwm)) => pca.set_pwm_off(pwm),
+                Ok(RTCommand::SetPwmOn(pwm)) => pca.set_pwm_on(pwm),
+                Ok(RTCommand::StopAllMotors) => pca.set_all_pwm_off(),
+                Ok(RTCommand::End) => return, //Main thread asked us to stop
+            } {
+                if let Err(_) = tx.send(Err(e)) { return; } // main dropped its rx
+            }
         }
         //Sync
         let elapsed = SystemTime::now().duration_since(time);
@@ -94,7 +178,7 @@ fn real_time_loop(mut pca: PCA9685, mut mpu: MPU6050,
     }
 }
 
-fn collect_data(mpu: &mut MPU6050, pca: &mut PCA9685, time: SystemTime) -> Result<SensorState, LinuxI2CError> {
+fn collect_data(mpu: &mut MPU6050, _pca: &mut PCA9685, time: SystemTime) -> Result<SensorState, LinuxI2CError> {
     let accel = mpu.get_accel_data(true)?;
     let gyro = mpu.get_gyro_data()?;
     let temp = mpu.get_temp()?;
